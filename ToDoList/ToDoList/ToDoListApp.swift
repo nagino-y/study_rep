988d06ea841dda5b3aa9996b7fc837d08db9c10b
Yukichi <naginoyukichi@gmail.com>
2023-11-15 18:35:34 +0900

    todolist

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by 奈木野諭吉 on 2023/11/15.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
