70ca5d34c945715260850557d352a8d9f74eb7f1
Yukichi <naginoyukichi@gmail.com>
2023-11-22 13:38:20 +0900

    newId definision add

---
+++ ToDoList/ToDoList/Views/NewItemView.swift
@@ -0,0 +1,61 @@
+//
+//  NewitemView.swift
+//  ToDoList
+//
+//  Created by 奈木野諭吉 on 2023/11/16.
+//
+
+import SwiftUI
+
+struct NewItemView: View {
+    @StateObject var viewModel = NewItemViewViewModel()
+    @Binding var newItemPresented: Bool
+    
+    var body: some View {
+        VStack{
+            Text("New Item")
+                .font(.system(size: 32))
+                .bold()
+                .padding(.top, 100)
+            
+            Form {
+                //Title
+                TextField("Title", text: $viewModel.title)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                //Due Date
+                DatePicker("Due Date", selection: $viewModel.dueDate)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                
+                //Button
+                TLButton(
+                    title: "Save",
+                    background: .pink
+                ) {
+                    if viewModel.canSave {
+                        viewModel.save()
+                        newItemPresented = false
+                    } else {
+                        viewModel.showAlert = true
+                    }
+
+                }
+                .padding()
+            }
+            .alert(isPresented: $viewModel.showAlert) {
+                Alert(title: Text("Error"),
+                      message: Text("Please fill in all fields and select due date that is today or newer."))
+            }
+        }
+    }
+}
+
+struct NewitemView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewItemView(newItemPresented: Binding(get: {
+            return true
+        }, set: { _ in
+            
+        }))
+    }
+}
